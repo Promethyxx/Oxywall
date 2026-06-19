feb7bde40006a50e516ec44be5101e2667596e65
Promethyxx <peter.mboyo@ik.me>
2026-06-19 21:32:48 +0200

    0.3.1

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 fn main() {
-    // Windows : attacher l'icône .ico à l'exécutable
+    // Windows : attacher l'icône .ico à l'exécutable + supprimer la console en release
     #[cfg(target_os = "windows")]
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/Oxywall_icon.ico");
         res.compile().unwrap();
+
+        println!("cargo:rustc-link-arg-bins=/SUBSYSTEM:WINDOWS");
+        println!("cargo:rustc-link-arg-bins=/ENTRY:mainCRTStartup");
     }
     // Sur Linux/macOS : rien à faire
 }
